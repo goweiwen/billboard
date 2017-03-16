ce4e4f49a1059121b002c7a8cc300031c1c0d721
Goh Wei Wen <goweiwen@gmail.com>
2017-03-16 15:22:14 +0800

    Switched from AutoResize to ResizableEditText
    Swipe to resize, long press to edit

---
+++ app/src/main/java/com/goweiwen/billboard/ResizableEditText.kt
@@ -0,0 +1,50 @@
+package com.goweiwen.billboard
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+
+/**
+ * Created by weiwen on 3/16/17.
+ */
+
+class ResizableEditText : EditText {
+    val _textSize = textSize
+    var scaleFactor = 1f
+
+    constructor(ctx: Context) : super(ctx)
+    constructor(ctx: Context, attrs: AttributeSet) : super(ctx, attrs)
+
+    private val scaleDetector = ScaleGestureDetector(this.context,
+            object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+                override fun onScale(detector: ScaleGestureDetector): Boolean {
+                    scaleFactor *= detector.scaleFactor
+                    scaleFactor = Math.max(0.1f, Math.min(scaleFactor, 3.0f))
+                    textSize = _textSize * scaleFactor
+                    return true
+                }
+            }
+    )
+
+    private val gestureDetector = GestureDetector(
+            object : GestureDetector.SimpleOnGestureListener() {
+                override fun onLongPress(e: MotionEvent?) {
+                    this@ResizableEditText.requestFocus()
+                    val imm = (context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+                    imm.showSoftInput(this@ResizableEditText, InputMethodManager.SHOW_IMPLICIT)
+                }
+            }
+    )
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        var res = scaleDetector.onTouchEvent(event)
+        if (!scaleDetector.isInProgress)
+            gestureDetector.onTouchEvent(event)
+        return res
+    }
+
+}
